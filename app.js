c33a9737045a4690d26c6e9a14e4834c6752a35d
Anugeeth <anugeeth2@gmail.com>
2019-12-22 11:34:33 +0530

    .

--- app.js
+++ app.js
@@ -2,15 +2,6 @@ console.log('Starting app....');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var user = os.userInfo().username;
-
-fs.appendFile('greetings.txt','hello world' + user + '!',
-function(err){
-    if(err)
-    {
-        console.log('could not write to file');
-    }
-}
-);
